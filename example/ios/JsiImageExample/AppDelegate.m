3de3a259eb0ae7febe7be694ee63088fef3b8655
Marc Rousavy <me@mrousavy.com>
2022-01-04 11:14:30 +0100

    Upgrade to RN 0.66

--- example/ios/JsiImageExample/AppDelegate.m
+++ example/ios/JsiImageExample/AppDelegate.m
@@ -20,7 +20,11 @@
                                                    moduleName:@"JsiImageExample"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  if (@available(iOS 13.0, *)) {
+    rootView.backgroundColor = [UIColor systemBackgroundColor];
+  } else {
+    rootView.backgroundColor = [UIColor whiteColor];
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
