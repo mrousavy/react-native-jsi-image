10cf08fdc5aa75167dc6071c2e3d0fabd3159cb3
Marc Rousavy <me@mrousavy.com>
2022-01-04 10:06:45 +0100

    Create module again

---
+++ ios/JsiImage.h
@@ -0,0 +1,15 @@
+//
+//  JsiImage.h
+//  JsiImage
+//
+//  Created by Marc Rousavy on 04.01.22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#pragma once
+
+#import <React/RCTBridgeModule.h>
+
+@interface JsiImage : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/JsiImage.mm
@@ -0,0 +1,18 @@
+//
+//  JsiImage.mm
+//  JsiImage
+//
+//  Created by Marc Rousavy on 04.01.22.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import "JsiImage.h"
+#import "ImageHostObject.h"
+
+@implementation JsiImage
+
+RCT_EXPORT_MODULE()
+
+// TODO: Inject JSI methods
+
+@end
